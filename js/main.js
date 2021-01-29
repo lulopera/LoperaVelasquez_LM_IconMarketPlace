3a061daf3f03f53ca05c6b90077c7e612c2e5f82
Lulopera <71723463+lulopera@users.noreply.github.com>
2021-01-29 15:39:37 -0500

    Javascript commands

--- js/main.js
+++ js/main.js
@@ -4,13 +4,4 @@
   //stub
   console.log('JS is linked! Hooray');
 
-  let theButton = document.querySelector(".icon-button");
-    theIcons = document.querySelectorAll(".icon");
-
-  function logClick() {
-    console.log('clicked the button!');
-  }
-
-  theButton.addEventListener('click', logClick);
-  theIcons.forEach(icon => icon.addEventListener('click', logClick));
 })();
